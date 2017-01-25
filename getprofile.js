8da98c365d181b51c4ca5e40d889fe14a1eba71f
山崎翔一 <s.zakiyama@gmail.com>
2017-01-25 18:16:36 +0900

    getTweet.phpとのやりとりする追記

--- getprofile.js
+++ getprofile.js
@@ -1,4 +1,6 @@
-//読み込み時にajax起動
+let myId;
+
+        //読み込み時にajax起動:profle dataを読み込む
 		$.ajax({
                 type:"GET",
                 // data:,
@@ -7,12 +9,33 @@
                 url:'./getProfileTest.php'
             }).done(function(data){
                     console.log(data);
-                    let abc = JSON.parse(data);
-                    console.log(abc);
-                    $("#my_name").attr('my_name',abc.user_name);
-                    $("#my_id").attr('my_id',abc.user_id);
-                    $('main').append(data);
+                    let pfD = JSON.parse(data);
+                    // console.log(abc);
+                    $("#my_name").attr('my_name',pfD.user_name);
+                    $("#my_id").attr('my_id',pfD.user_id);
+                    myId = pfD.id;
+                    console.log(myId);
+
+
+                    //個人のtweetdataを読み込む
+                    $.ajax({
+                        type:'GET',
+                        url:'./getTweet.php',
+                        data:{
+                            'user_id':myId
+                        }
+                    }).done(function(data){
+                        // console.log(data);
+                        let twD = JSON.parse(data);
+                        console.dir(twD[0].tw_text);
+
+                    }).fail(function(data){
+                        console.log('error')
+                    });
             })
             .fail(function(data){
                     console.log(data);
         });
+        /*---------------------------------------------------------*/
+
+        
